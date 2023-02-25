cd3239f42f866b536d846bee9068b85185078c8a
Patrik Csak <p@trikcsak.com>
2023-02-24 17:33:33 -0800

    show top 20 instead of 10

--- src/hooks/use-top-mastodon-statuses.ts
+++ src/hooks/use-top-mastodon-statuses.ts
@@ -18,7 +18,7 @@ export default function useTopMastodonStatuses({
 	if (topStatuses) {
 		topStatuses = topStatuses.filter((status) => status.favourites_count > 0);
 		topStatuses.sort((a, b) => b.favourites_count - a.favourites_count);
-		topStatuses = topStatuses.slice(0, 10);
+		topStatuses = topStatuses.slice(0, 20);
 	}
 
 	return { isLoading, progress, topStatuses };
